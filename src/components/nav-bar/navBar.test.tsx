9edea5382cfdcdadcb444769e9e94d3eea6240c0
jesus1811 <jayarza1811@gmail.com>
2022-08-26 11:45:45 -0500

    add navBar test

---
+++ src/components/nav-bar/navBar.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import NavBar from "./NavBar";
+
+describe("NavBar.tsx", () => {
+  test("renders content", () => {
+    const component = render(<NavBar />);
+    component.getByText("Rickvana");
+  });
+});
